baf1aa5adf4421a212a5ddafcaba88b2c8d88237
Christopher Deutsch <cd@cdeutsch.com>
2013-03-21 09:14:46 -0500

    Checkin of version 1.0.0

---
+++ Content/App_Start/Bootstrap.BundleConfig.cs.pp
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+using BundleTransformer.Core.Transformers;
+using BundleTransformer.Core.Orderers;
+
+namespace $rootnamespace$
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //// use this BundleConfig OR add the 2 lines below to your existing BundleConfig.cs and delete this file.
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.less"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+        }
+    }
+}
